0a9233c8f18dcde55a038cffc1a0a5361fded6c9
Bilyalov <eldar2117@gmail.com>
2025-02-22 00:51:26 +0300

    first commit

---
+++ src/OzonEdu.StockApi/Program.cs
@@ -0,0 +1,13 @@
+// Содержит классы и интерфейсы для запуска и настройки веб-приложения
+
+using Microsoft.AspNetCore.Hosting;
+// Инфраструктура для управления жизненным циклом приложения
+using Microsoft.Extensions.Hosting;
+using OzonEdu.StockApi;
+
+
+CreateHostBuilder(args).Build().Run();
+
+static IHostBuilder CreateHostBuilder(string[] args)
+    => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(
+        webBuilder => { webBuilder.UseStartup<Startup>(); });
